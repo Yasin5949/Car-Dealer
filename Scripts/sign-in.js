5910a5df44d7442c05be808f120e378a134f5f70
Yasin5949 <yasin2ashalo@gmail.com>
2025-03-02 15:32:04 -1200

    first commit

---
+++ Scripts/sign-in.js
@@ -0,0 +1,9 @@
+let go=document.querySelector(".back");
+let user=document.querySelector(".username");
+let pass=document.querySelector(".password");
+function sign(){
+    if(user.value==="Yasin" && pass.value==="1234"){
+        go.innerHTML="Logged IN";
+    }
+
+}
