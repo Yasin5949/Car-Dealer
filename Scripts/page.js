cec27d7935190bf30029bc781304338d72594c42
Yasin5949 <yasin2ashalo@gmail.com>
2025-03-06 00:09:39 -1200

    Added other page like admin login page and so

--- Scripts/page.js
+++ Scripts/page.js
@@ -1,5 +1,22 @@
-
+let settingForm=document.querySelector(".panel");
+let showSetting=true;
 function setting(){
-    let setPanel=querySelector(".Panel");
-    setPanel.innerHTML="setting-panel";
+    
+    let apeared=document.querySelector(".setting");
+    
+    if(showSetting){
+        showSetting=false;
+        settingForm.classList.add("setting-panel");
+        settingForm.innerHTML=`<a href='homePage.html'><button>Catagory</button></a>
+                                <a href='homePage.html'><button>Trending</button></a>
+                                <a href='homePage.html'><button>Setting</button></a>
+                                <a href='homePage.html'><button>About</button></a>
+                                <a href='homePage.html'><button>Logout</button></a>
+                                <a href='homePage.html'><button>Support</button></a>`;
+    }
+    else{
+        showSetting=true;
+        settingForm.classList.remove("setting-panel");
+        settingForm.innerHTML='';
+    }
 }
--- Scripts/sign-in.js
+++ Scripts/sign-in.js
@@ -1,9 +1,16 @@
-let go=document.querySelector(".back");
+let go=document.querySelector(".logged");
 let user=document.querySelector(".username");
 let pass=document.querySelector(".password");
 function sign(){
     if(user.value==="Yasin" && pass.value==="1234"){
-        go.innerHTML="Logged IN";
+        go.innerHTML="Logged In Successfully";
+        setTimeout(function(){
+            window.location.href="page.html";
+        },10);
+    }
+    else{
+        go.innerHTML='Incorrect Credential';
+        go.style.color="red";
     }
 
 }
