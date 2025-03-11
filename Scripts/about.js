5a6f0a4a3d7cce929c2c1a5a487b417b806e6eae
Yasin5949 <yasin2ashalo@gmail.com>
2025-03-11 03:58:36 -1200

    made modification to the welcome home page and about page

--- Scripts/about.js
+++ Scripts/about.js
@@ -1,16 +1,74 @@
 let next = document.querySelector('.next');
 let prev = document.querySelector('.prev');
-
+const about=document.querySelector(".yasinInfo");
+const aboutSecond=document.querySelector(".frontDev");
+const picture=document.querySelector(".yasin");
+const aboutdev=document.querySelector(".secondDeveloper");
+const progress=document.querySelector(".mine");
+const progressTwo=document.querySelector(".collaborator");
+let count=0;
 next.addEventListener('click', function(){
+    count=count+1;
     let items = document.querySelectorAll('.item');
     document.querySelector('.slide').appendChild(items[0]);
     updateActiveClass();
+    updateFirst();
+    if(count===3){
+        count=-2;
+    }
+  
+    console.log(count);
+    if(count===0 || count===1 || count===2){
+        aboutdev.classList.remove('currentlly');
+        aboutSecond.classList.remove('currentlly');
+        picture.classList.remove('currentOne');
+        about.classList.remove('currentOne');
+        progress.classList.add('show');
+        progressTwo.classList.remove('show');
+    }
+    else{
+        aboutdev.classList.add('currentlly');
+        aboutSecond.classList.add('currentlly');
+        picture.classList.add('currentOne');
+        about.classList.add('currentOne');
+        progress.classList.remove('show');
+        progressTwo.classList.add('show');
+
+    }
+    
+    
+
 })
 
 prev.addEventListener('click', function(){
+    count=count-1;
     let items = document.querySelectorAll('.item');
     document.querySelector('.slide').prepend(items[items.length - 1]);
     updateActiveClass();
+    updateFirst();
+    if(count===-3){
+        count=2;
+    }
+    console.log(count);
+    if(count===-1 ||  count===-2){
+        aboutdev.classList.add('currentlly');
+        aboutSecond.classList.add('currentlly');
+        picture.classList.add('currentOne');
+        about.classList.add('currentOne');
+        progressTwo.classList.add('show');
+    }
+    else{
+        aboutdev.classList.remove('currentlly');
+        aboutSecond.classList.remove('currentlly');
+        picture.classList.remove('currentOne');
+        about.classList.remove('currentOne');
+        progressTwo.classList.remove('show');
+
+    }
+    
+    
+    
+
 })
 
 function updateActiveClass() {
@@ -19,6 +77,17 @@ function updateActiveClass() {
     let middleIndex = Math.floor(items.length / 2);
     items[middleIndex].classList.add('active');
 }
+function updateFirst(){
+ let items=document.querySelectorAll('.item');
+ items.forEach(item=> item.classList.remove('far'));
+ let firstIndex=items.length-5;
+ let last=items.length-1;
+ items[firstIndex].classList.add('far');
+ items[last].classList.add('far');
+}
+
+   
+updateFirst();
 
 updateActiveClass();
 
---
+++ Scripts/home.js
@@ -0,0 +1,70 @@
+
+
+function move(){
+    setInterval(()=>{
+        let moving=document.querySelectorAll(".sliders");
+        document.querySelector('.slide').appendChild(moving[0]);
+        updateFirst();
+        middle();
+        updateActiveClass();
+    },300);
+}
+function updateActiveClass() {
+    let moving = document.querySelectorAll('.sliders');
+    moving.forEach(item => item.classList.remove('active'));
+    let middleIndex = Math.floor(moving.length / 2);
+    moving[middleIndex].classList.add('active');
+
+}
+function updateFirst(){
+    let items=document.querySelectorAll('.sliders');
+    items.forEach(item=> item.classList.remove('far'));
+    let firstIndex=items.length-9;
+    let last=items.length-1;
+    items[firstIndex].classList.add('far');
+    items[last].classList.add('far');
+   }
+function middle(){
+    let items=document.querySelectorAll('.sliders');
+    items.forEach(item=> item.classList.remove('middle'));
+    let second=items.length-8;
+    let lastsecond=items.length-2;
+    items[second].classList.add('middle');
+    items[lastsecond].classList.add('middle');
+}
+function display(){
+    setInterval(()=>{
+
+   let n=Math.floor(Math.random()*4)+1;
+   console.log(n);
+   if(n===1){
+        document.querySelector('.nameshow').classList.add('animateTwo');
+        setTimeout(()=>{
+            document.querySelector('.nameshow').classList.remove('animateTwo');
+        },10000);
+   }
+   else if(n===3){
+        document.querySelector('.nameshow').classList.add('animateTwo');
+        setTimeout(()=>{
+            document.querySelector('.nameshow').classList.remove('animateTwo');
+        },10000);
+   }
+   else if(n===2){
+    document.querySelector('.nameshowTwo').classList.add('animate');
+    setTimeout(()=>{
+        document.querySelector('.nameshowTwo').classList.remove('animate');
+    },10000);
+   }
+   else if(n===4){
+    document.querySelector('.nameshowTwo').classList.add('animate');
+      setTimeout(()=>{
+        document.querySelector('.nameshowTwo').classList.remove('animate');
+      },10000);
+   }
+    },15000);
+}
+move();
+display();
+updateFirst();
+middle();
+updateActiveClass() ;
